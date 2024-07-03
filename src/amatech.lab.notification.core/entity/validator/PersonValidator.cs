20255f6cbe18c933f5f9a62d1ab6910ce5f3ed57
AMFSouza <amfdeveloper@gmail.com>
2024-07-03 02:26:44 -0300

    fix to show English messages

--- src/amatech.lab.notification.core/entity/validator/PersonValidator.cs
+++ src/amatech.lab.notification.core/entity/validator/PersonValidator.cs
@@ -6,8 +6,8 @@ public class PersonValidator : ValidatorInterface<Person>
     public Notification Validate(Person source)
     {
         Notification notification = new Notification();
-        if ( String.IsNullOrEmpty(source.Name)) notification.AddError(new MessageError(source.Name, nameof(source.Name), "Nome deve ser informado"));
-        if ( source.Age.ToString() is null || source.Age < 18) notification.AddError(new MessageError(source.Age.ToString(), nameof(source.Age), "Idade abaixo do permitido"));
+        if ( String.IsNullOrEmpty(source.Name)) notification.AddError(new MessageError(source.Name, nameof(source.Name), "Name should not be empty"));
+        if ( source.Age.ToString() is null || source.Age < 18) notification.AddError(new MessageError(source.Age.ToString(), nameof(source.Age), "Age should be greater than 18"));
         return notification;
     }
 }
